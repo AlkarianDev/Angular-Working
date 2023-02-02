1ae86ebc2ff5d130318c14e7797263613abcf4a0
Korolczuk <alkariandev@gmail.com>
2023-02-02 08:51:23 +0100

    push

--- src/app/models/NewTimeEnter.model.ts
+++ src/app/models/NewTimeEnter.model.ts
@@ -1,30 +1,29 @@
-import { Time } from "@angular/common";
 import { Chantier } from "./chantier.model";
 
 export class NewTimeEnter {
 
   constructor(
     //deprecated
-  //public id: number,
-  // Add time in hours start end with calc version ?
-  // Could be nice
-  // Easy Version put only hours
-  //Hard version put start and end time (maybe a break time inside) (enter in Chantier and leave + break which will be the -30 min for exemple)
+    //public id: number,
+    // Add time in hours start end with calc version ?
+    // Could be nice
+    // Easy Version put only hours
+    //Hard version put start and end time (maybe a break time inside) (enter in Chantier and leave + break which will be the -30 min for exemple)
 
 
-  // Delete Name as not needed.
-  public startTime: any,
-  public endTime: any,
+    // Delete Name as not needed.
+    public startTime: any,
+    public endTime: any,
 
-  //date should be retrive // for now it is hard code.
-  public date: any,
-  //Comment peut etre optionel !
-  //public comment: string,
+    //date should be retrive // for now it is hard code.
+    public date: any,
+    //Comment peut etre optionel !
+    //public comment: string,
 
-  // Il faudra prevoir des truc generique pas
-  public chantier: Chantier
+    // Il faudra prevoir des truc generique pas
+    public chantier: Chantier
 
-  ) {}
+  ) { }
 
 
 }
